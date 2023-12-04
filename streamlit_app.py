0528185099e19c796e9a67de04768ca922cd91dd
Prithvi-Pai <paiprithvi93@gmail.com>
2023-12-04 21:18:08 +0530

    Update streamlit_app.py
    indentation correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ def insert_row_Snowflake(new_fruit):
         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
         return "Thanks for adding",+add_fruit
 add_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-    if streamlit.button('Add a fruit to the list'):
+if streamlit.button('Add a fruit to the list'):
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         back_from_function = insert_row_snowflake(add_my_fruit)
         streamlit.text(back_from_function)
