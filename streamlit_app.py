d081819f12d369c2dc8c154893893fcd1d506b66
Prithvi-Pai <paiprithvi93@gmail.com>
2023-12-04 20:59:24 +0530

    Update streamlit_app.py
    create a recursive function

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,15 +19,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select the fruit to get info:")
-  else:
-#streamlit.write('The user entered ', fruit_choice)
 
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
+#creating function
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
 #streamlit.text(fruityvice_response.json())
 #normalise the json output
 # dont run anything past here while we troubleshoot
@@ -36,7 +32,16 @@ try:
 
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display output to screen
-  streamlit.dataframe(fruityvice_normalized)
+  #streamlit.dataframe(fruityvice_normalized)
+    return fruityvice_normalized
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select the fruit to get info:")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
